33934c5a489b80baa38edb84657397fc1e5bfad9
Mikkel Munkholm Blak Nilsson <muni@itu.dk>
2023-01-09 18:52:29 +0100

    setup working project

---
+++ README.md
@@ -0,0 +1,12 @@
+# AnoPy - A discord bot
+
+This bot is a project for me to learn more about python.
+Get the bot for your server [here](https://discord.com/api/oauth2/authorize?client_id=1062042796871135282&permissions=3213376&scope=bot).
+
+Plans:
+ - Create custom decorators for commands
+ - Setup DB to keep track of guild data
+
+
+Future:
+ - Create a tts command which can connect to VC and read what ever is written to it.
--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-import discord.py
+import discord
+
+import os
+from dotenv import load_dotenv
+
+import commands
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message: discord.Message):
+        if message.author.bot:
+            return
+            
+        print(f'{message.guild.name}#{message.channel.name} - {message.author}: {message.content}')
+        await message.channel.send("Hello World!")
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents, command_prefix='!', help_command=None, tree_cls=commands.MyCommandTree)
+
+client.run("MTA2MjA0Mjc5Njg3MTEzNTI4Mg.Guvvo5.TBYDsdKJthCu2ak_9spBVZ_ECiK3pRtIWBgqv4")
