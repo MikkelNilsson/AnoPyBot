19b8894d87ef4cb22f4ba479f3f37c26a665ecb3
Mikkel Munkholm Blak Nilsson <muni@itu.dk>
2023-04-21 16:31:01 +0200

    Setup

---
+++ config.py
@@ -0,0 +1,52 @@
+import sys
+import json
+import logging
+import discord
+from main import MyClient
+
+
+def configLog(client: MyClient):
+    logger = logging.getLogger("discord")
+    logger.setLevel(logging.INFO)
+    logging.getLogger("discord.http").setLevel(logging.INFO)
+
+    handler = logging.handlers.RotatingFileHandler(
+        filename="./logs/discord.log",
+        encoding="utf-8",
+        maxBytes=32 * 1024 * 1024,  # 32 MiB
+        backupCount=5,  # Rotate through 5 files
+    )
+
+    dt_fmt = "%d/%m/%Y %H:%M:%S"
+    formatter = logging.Formatter(
+        "[{asctime}] [{levelname:<8}] {name}: {message}", dt_fmt, style="{"
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    std_err = logging.StreamHandler()
+    std_err.setFormatter(formatter)
+    logger.addHandler(std_err)
+
+    client.logger = logger
+
+
+def load_config():
+    if len(sys.argv) > 1 and sys.argv[1].lower() == "prod":
+        print("Loading production config")
+        with open("config.json", "r") as f:
+            return json.load(f)
+    else:
+        print("Loading development config")
+        with open("config-development.json", "r") as f:
+            return json.load(f)
+
+
+def config():
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    client = MyClient(intents=intents)
+    client.config = load_config()
+    configLog(client)
+
+    return client
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import discord
-
-import os
-from dotenv import dotenv_values
+import config
 import logging
 import logging.handlers
 
 
 class MyClient(discord.Client):
     def __init__(self, intents: discord.Intents):
-        self.env: dict[str, str | bool | None] = None
+        self.config = None
         self.logger: logging.Logger = None
         super().__init__(intents=intents)
 
@@ -19,48 +17,12 @@ class MyClient(discord.Client):
         if message.author.bot:
             return
         self.logger.info(
-            f"{message.guild.name}/{message.channel.name} - {message.author}: {message.content}",
+            f"{message.guild.name}/{message.channel.name} - {message.author}: {message.content}"
         )
 
         await message.channel.send("Hello World!")
 
 
-def configLog(client: MyClient):
-    logger = logging.getLogger("discord")
-    logger.setLevel(logging.INFO)
-    logging.getLogger("discord.http").setLevel(logging.INFO)
-
-    handler = logging.handlers.RotatingFileHandler(
-        filename="./logs/discord.log",
-        encoding="utf-8",
-        maxBytes=32 * 1024 * 1024,  # 32 MiB
-        backupCount=5,  # Rotate through 5 files
-    )
-
-    dt_fmt = "%d/%m/%Y %H:%M:%S"
-    formatter = logging.Formatter(
-        "[{asctime}] [{levelname:<8}] {name}: {message}", dt_fmt, style="{"
-    )
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    std_err = logging.StreamHandler()
-    std_err.setFormatter(formatter)
-    logger.addHandler(std_err)
-
-    client.logger = logger
-
-
-def config():
-    intents = discord.Intents.default()
-    intents.message_content = True
-
-    client = MyClient(intents=intents)
-    client.env = dotenv_values(".env")
-    configLog(client)
-
-    token = client.env.get("DISCORD_TOKEN")
-    client.run(token, log_handler=None)
-
-
 if __name__ == "__main__":
-    config()
+    client = config.config()
+    client.run(client.config["discord"]["token"], log_handler=None)
